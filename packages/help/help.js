927be17885609fe5c68e6e70eca1c761ccd64b43
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-09-21 19:25:20 -0400

    feat(help): Add help command to your application
    BREAKING CHANGE: Initial

---
+++ packages/help/help.js
@@ -0,0 +1,65 @@
+import { map, filter, concat } from '@northbrook/util'
+import { app } from '@ragnar/app'
+import { command } from '@ragnar/command'
+import { alias } from '@ragnar/alias'
+import { run } from '@ragnar/run'
+import { asStream } from '@ragnar/stream'
+
+export function help (name, ...definitions) {
+  if (typeof name !== 'string') {
+    throw new Error('help must be given a name as its first argument')
+  }
+
+  return app(...definitions, buildHelpCommand(name, definitions))
+}
+
+function buildHelpCommand (name, definitions) {
+  const helpCommand = command(alias('help'))
+
+  const displayHelp = showHelp(name, definitions)
+
+  asStream(helpCommand).observe((app) => {
+    if (app.args.length > 0) {
+      const name = app.args.shift()
+      run(['help', ...app.args], help(name, app))
+    } else {
+      displayHelp(app, '')
+    }
+  })
+
+  return helpCommand
+}
+
+function showHelp (name, definitions) {
+  const apps = filterApp(definitions)
+  const cmds = concat(filterCommands(definitions), map(x => x.command, apps))
+  const flags = concat(filterFlag(definitions), map(x => x.flag, apps))
+
+  return function ({ args, options }, description) {
+    console.log(`  ${name.toUpperCase()}
+
+  COMMANDS:
+
+    ${cmds.map(displayCommand)}
+
+  FLAGS:
+
+    ${flags.map(displayFlag)}
+`)
+  }
+}
+
+function displayCommand ({ alias, desc }) {
+  return `${alias[0][0]} ${desc ? '- ' + desc : ''}\n    `
+}
+
+function displayFlag ({ alias, desc }) {
+  return `${alias[0][0]} ${desc ? '- ' + desc : ''}\n    `
+}
+
+const filterType = type => definitions =>
+  filter(x => x.type === type && !!x.desc && x.alias && x.alias[0][0], definitions)
+
+const filterApp = filterType('app')
+const filterCommands = filterType('command')
+const filterFlag = filterType('flag')
